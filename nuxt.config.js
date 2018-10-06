4e8c3a04a118447ca564cb28def4b532307de9c9
epaew <epaew.333@gmail.com>
2018-10-06 16:12:12 +0000

    add around css

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  modules: [["@nuxtjs/google-analytics", { id: "UA-127066891-1" }]]
+  head: {
+    titleTemplate: "%s - epaew's home",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width,initial-scale=1.0" }
+    ]
+  },
+  css: [
+    "purecss/build/menus-min.css",
+    "@/assets/css/skyblue/css/skyblue.min.css",
+    "@/assets/sass/app.scss"
+  ],
+  modules: [["@nuxtjs/google-analytics", { id: "UA-127066891-1" }]],
+  build: { extractCSS: { allChunks: true } }
 };
