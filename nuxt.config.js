79d60e472a65fb629493a8743f66213b0510a18e
epaew <epaew.333@gmail.com>
2018-10-28 06:11:07 +0000

    add favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,8 @@ module.exports = {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width,initial-scale=1.0" }
-    ]
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
   css: [
     "purecss/build/menus-min.css",
