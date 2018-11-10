054034232a61697ebaa00259bbe67a1fec3b0245
epaew <epaew.333@gmail.com>
2018-11-10 03:29:38 +0000

    update index page layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,16 @@
 <template lang="pug">
-.row.margin-10
-  .col
-    h3 About
-    p 都内でアプリケーションエンジニアをしている、epaewの個人サイトです
-  .col.md-8
-    h3 Blog
-    p Now building...
-  .col.md-4
-    h3 Twitter
-    Timeline(id="epaew_dev" sourceType="profile" :options="{ tweetLimit: '5' }")
+  .row.margin-10
+    .col.md-8
+      .row.margin-0
+        h3 About
+        p 都内でアプリケーションエンジニアをしている、epaewの個人サイトです
+      .row.margin-0
+        h3 Blog
+        p Now building...
+    .col.md-4
+      .row.margin-0
+        h3 Twitter
+        Timeline(id="epaew_dev" sourceType="profile" :options="{ tweetLimit: '5' }")
 </template>
 
 <script>
