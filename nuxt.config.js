6da97d7955b7106d88349dba5cfe29ef65beadce
epaew <epaew.333@gmail.com>
2020-03-13 07:17:21 +0900

    Update configs to use Nuxt.js with TypeScript
    https://typescript.nuxtjs.org/ja/guide/setup.html

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,9 +35,8 @@ export default {
    ** Nuxt.js dev-modules
    */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxt/typescript-build',
     '@nuxtjs/eslint-module',
-    // Doc: https://github.com/nuxt-community/stylelint-module
     '@nuxtjs/stylelint-module',
     '@nuxtjs/vuetify'
   ],
