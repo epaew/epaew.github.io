8e068730a858b7175daff238fba7af6a603d0857
epaew <epaew.333@gmail.com>
2020-03-28 14:13:13 +0900

    import google fonts in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,15 @@ export default {
     htmlAttrs: {
       lang: 'ja',
     },
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        type: 'text/css',
+        href:
+          'https://fonts.googleapis.com/css?family=Allura|Dancing+Script|Noto+Sans+JP&display=swap',
+      },
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
