4b0aa6f95ce13cf0b9d694d47563ebb45a6bb6f4
epaew <epaew.333@gmail.com>
2021-03-07 15:57:01 +0900

    Update components/molecules

--- src/components/molecules/app-title.tsx
+++ src/components/molecules/app-title.tsx
@@ -1,6 +1,11 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
-const Typography = styled.h1();
+const Typography = styled.h1(({ theme }) => ({
+  color: '#888',
+  fontFamily: theme.fontFamily.appTitle,
+  fontSize: theme.fontSize['48'],
+  margin: 0,
+}));
 
 export const AppTitle: FC = () => <Typography>epaew.net</Typography>;
--- src/components/molecules/copyright.tsx
+++ src/components/molecules/copyright.tsx
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 import { FC } from 'react';
 
-const Typography = styled.p();
+const Typography = styled.p(({ theme }) => ({
+  fontSize: theme.fontSize['12'],
+  margin: theme.spacing(1, 2),
+}));
 
 export const Copyright: FC = () => <Typography>&copy; 2018 Ryo MAEDA</Typography>;
