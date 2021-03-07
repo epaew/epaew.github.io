c10a00854155b426dc007fbaf70e35a4c5925eff
epaew <epaew.333@gmail.com>
2021-03-07 11:47:29 +0900

    use ThemeProvider of @emotion/react

---
+++ src/@types/emotion.d.ts
@@ -0,0 +1,7 @@
+import { theme } from '../theme';
+
+type AppTheme = typeof theme;
+
+declare module '@emotion/react' {
+  export interface Theme extends AppTheme {}
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,12 @@
+import { ThemeProvider } from '@emotion/react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
+import { theme } from '../theme';
+
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Head>
         <title>epaew.net</title>
         <link rel="icon" href="/favicon.ico" />
@@ -13,7 +16,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
       </main>
       <footer>
       </footer>
-    </>
+    </ThemeProvider>
   );
 };
 
