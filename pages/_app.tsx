f2c1be1e40e14d476ae4e64f3aa8624622c0d4fc
epaew <epaew.333@gmail.com>
2021-03-07 10:59:02 +0900

    Update pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      <Head>
+        <title>epaew.net</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <Component {...pageProps} />
+      </main>
+      <footer>
+      </footer>
+    </>
+  );
+};
 
-export default MyApp
+export default MyApp;
